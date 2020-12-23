9ae271cd243f4608600a916e22602fc7c698ac51
Stepan Prucha <69053043+StepanPrucha@users.noreply.github.com>
2020-12-23 14:35:07 +0100

    Add files via upload

--- flowdesign/script.js
+++ flowdesign/script.js
@@ -1,14 +1,14 @@
 function thanks() {
   var thx = document.getElementById("thanks");
   var body = document.getElementById("body")
-  if (thx.style.display === "block") {
-    thx.style.display = "none";
-    body.style.height = "auto"
-    body.style.overflow = "auto"
+  if (thx.style.display === "flex") {
+    thx.style.display = null;
+    body.style.height = null;
+    body.style.overflow = null;
   } else {
-    thx.style.display = "block";
-    body.style.height = "100%"
-    body.style.overflow = "hidden"
+    thx.style.display = "flex";
+    body.style.height = "100%";
+    body.style.overflow = "hidden";
   }
 }
 
@@ -16,7 +16,7 @@ $('a[href^="#"]').click(function(e){
   e.preventDefault();
   var target = $($(this).attr('href'));
   if(target.length){
-    var scrollTo = target.offset().top - 10;
+    var scrollTo = target.offset().top - 0;
     $('body, html').animate({scrollTop: scrollTo+'vh'}, 1000);
   }
 });
