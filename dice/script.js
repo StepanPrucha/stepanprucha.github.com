cae228e9a3727ef892be6bb35e34baec168dafad
Stepan Prucha <69053043+StepanPrucha@users.noreply.github.com>
2021-09-30 13:33:15 +0200

    Update script.js

--- dice/script.js
+++ dice/script.js
@@ -1,10 +1,3 @@
-const appHeight = () => {
-    const doc = document.documentElement
-    doc.style.setProperty('--app-height', `${window.innerHeight}px`)
-}
-window.addEventListener('resize', appHeight)
-appHeight()
-
 var body = document.getElementById("body");
 
 function myFunction() {
