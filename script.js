45a4c0f579afbeac2960c24190516240c0416ac8
Stepan Prucha <69053043+StepanPrucha@users.noreply.github.com>
2022-02-06 11:35:54 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-$('a[href^="#"]').click(function(e){
-  e.preventDefault();
-  var target = $($(this).attr('href'));
-  if(target.length){
-    var scrollTo = target.offset().top;
-    $('body, html').animate({scrollTop: scrollTo+'px'}, 500);
-  }
-});
